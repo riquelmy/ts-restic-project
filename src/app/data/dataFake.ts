76b62989258f7080585b695fea57419c8c87d1a8
Riquelmy <riquelmyor@gmail.com>
2024-08-24 22:22:34 -0300

    :sparkles: feat: I have finished the basic functionalities of the website, as well as the technology articles section

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,39 @@
+export const dataFake = [
+{
+ "id": "1",
+ "photo": "https://i.imgur.com/iE0brtw.png",
+ "title": `Trilha Full-Stack da residência RESTIC36 – CEPEDI`,
+ "description": `
+ A Residência em TIC 36 - Empreendedorismo + Bahia + Tecnologia é um programa inovador que visa capacitar profissionais para o cenário tecnológico atual, com um forte enfoque em empreendedorismo e inovação. Dentro deste programa, a trilha de Desenvolvimento Full-Stack oferece uma formação abrangente e detalhada para quem deseja se tornar um especialista completo em desenvolvimento web. Com 100 vagas disponíveis, essa trilha abrange todas as facetas da criação de aplicações web, desde o design e a implementação da interface do usuário (front-end) até a construção e gerenciamento dos servidores e bancos de dados (back-end). Os participantes aprenderão a integrar essas duas áreas, desenvolvendo habilidades práticas em diversas tecnologias e frameworks essenciais. Esta formação permitirá que os residentes construam soluções digitais integradas e escaláveis, capacitando-os para enfrentar desafios complexos e se destacar no mercado de tecnologia. O programa não só fornece conhecimento técnico, mas também incentiva o pensamento empreendedor e a aplicação prática das habilidades adquiridas, preparando os profissionais para liderar e inovar no setor tecnológico.`
+},
+{
+  "id": "2",
+  "photo": "https://i.imgur.com/Llip2Py.png",
+  "title": `CrowdStrike e Microsoft: o que causou o apagão?`,
+  "description": `O recente apagão digital que afetou milhares de empresas ao redor do mundo trouxe à tona questões críticas sobre a segurança cibernética, com a CrowdStrike e a Microsoft no centro do debate. A interrupção começou com uma vulnerabilidade em um dos sistemas de autenticação de identidade da Microsoft, que foi rapidamente explorada por cibercriminosos. Isso gerou uma cascata de falhas que impactaram desde pequenas startups até grandes corporações, revelando fragilidades nas infraestruturas de nuvem e segurança. A CrowdStrike, conhecida por suas soluções de cibersegurança avançadas, entrou em cena para investigar o incidente, identificando falhas no protocolo de segurança e destacando a necessidade de uma cooperação mais estreita entre as empresas de tecnologia e segurança. Esse incidente não apenas expôs a vulnerabilidade das infraestruturas digitais, mas também levantou importantes questões sobre a responsabilidade e a preparação das gigantes tecnológicas em prevenir e responder a ataques cibernéticos em escala global. A colaboração entre a CrowdStrike e a Microsoft agora é observada de perto, enquanto as empresas buscam mitigar danos futuros e restaurar a confiança dos usuários na segurança digital.`
+ },
+ {
+  "id": "3",
+  "photo": "https://i.imgur.com/pATk82A.png",
+  "title": "Entre Dados e Destino: A Revolução da Inteligência Artificial na Encruzilhada Humana",
+  "description": `A inteligência artificial (IA) está no centro de uma revolução que redefine a relação entre dados e destino humano, exigindo uma profunda reflexão sobre o papel da tecnologia em nossas vidas. À medida que algoritmos avançados analisam grandes volumes de dados, influenciando desde diagnósticos médicos até decisões judiciais, somos forçados a confrontar questões éticas sobre autonomia, controle e responsabilidade. Essa encruzilhada não é apenas tecnológica, mas profundamente humana, pois desafia nossas noções de livre-arbítrio e moralidade. Enquanto a IA oferece oportunidades sem precedentes para inovação e eficiência, ela também impõe a necessidade de uma reflexão crítica sobre como desejamos que ela molde nosso futuro. Nesse processo, é fundamental garantir que a automação complemente, e não substitua, a humanidade, preservando o que há de mais essencial em nossa experiência coletiva.`
+ },
+ {
+  "id": "4",
+  "photo": "https://i.imgur.com/zBtuzGM.png",
+  "title": "Como a Computação Transforma o Mundo Moderno",
+  "description": `
+  A influência da ciência da computação vai além do simples uso de tecnologia; ela redefine o que é possível em praticamente todos os campos do conhecimento. Na saúde, por exemplo, algoritmos sofisticados analisam vastos conjuntos de dados médicos para detectar doenças precocemente e personalizar tratamentos, salvando vidas. Na economia, eles otimizam mercados financeiros, prevendo tendências e riscos com uma precisão que seria impossível manualmente. No campo da comunicação, a lógica computacional permite a criação de redes sociais que conectam bilhões de pessoas globalmente, transformando a maneira como nos relacionamos e consumimos informações.
+  
+  A matemática e a lógica, bases fundamentais da computação, permitem a construção de modelos e simulações que testam cenários complexos, desde previsões climáticas até o comportamento de sistemas sociais. Com a capacidade de processar e analisar dados em grande escala, a ciência da computação oferece insights que impulsionam inovações constantes, tornando as operações mais eficientes e as decisões mais informadas. Em um mundo cada vez mais interconectado e orientado por dados, o domínio desses princípios se torna essencial não apenas para o desenvolvimento tecnológico, mas também para a resolução de problemas globais, como mudanças climáticas, segurança cibernética e sustentabilidade.`
+ },
+ {
+  "id": "0",
+  "photo": "../../../assets/1.jpg",
+  "title": "Sobre o projeto:",
+  "description": `O projeto em questão foi desenvolvido por mim (Riquelmy Oliveira de Ribeiro, 22 anos, Cientista da Computação em formação e desenvolvedor de software) para o módulo de TypeScript do RESTIC 36, trilha Full-Stack. Nesse aspecto, apliquei todos
+  os fundamentos ensinados até então (CSS, HTML, JavaScript, TypeScript). E, além disso, como um desafio, apliquei os conceitos de componentização, SPA, routerlinks, lifecycle, manipulação de DOM, etc, com o Angular. Nesse aspecto, solidifiquei o meu conhecimento e revisei a framework.`
+ },
+ 
+
+]
